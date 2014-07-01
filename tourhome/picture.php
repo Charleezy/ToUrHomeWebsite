c31da1773d5136c9dca802561ab23c11182bc98d
Charlie G <obesechicken13@yahoo.com>
2014-06-30 20:44:32 -0400

    Fixed image uploading
    Just made it so we use session data to get username for image uploading

--- tourhome/picture.php
+++ tourhome/picture.php
@@ -7,14 +7,15 @@ if($_FILES['file1']['name']!=''){
 }
 $destination="upload/".$_FILES['file1']['name'];
 $UserName="";
-if(isset($_POST["username"])){$UserName=$_POST["username"];}
+if(isset($_SESSION['username'])){$UserName=$_SESSION['username'];}
 $connect = mysql_connect("mysql.tour-home.org", "stevenpakfunglau", "libertinelux")  or die("Couldn't connect to the database");
 mysql_select_db("pakfung_phplogin");
 
 #echo "<script type='text/javascript'>alert(' destination {". $destination . "}');</script>";
+#Note: There is a nuance with updating a table when you are selecting it in a nested query in MySQL. To get around this you use "select * from profile as p" instead of just "profile"
 $update_profile_image_query = sprintf("update profile set image='%s' where id in (select id from users natural join (select * from profile) as p where username = '%s')", $destination, $UserName);
 #Currently not getting username because we're using sessions to see if users are logged in
-echo "<script type='text/javascript'>alert({". $update_profile_image_query . "});</script>";
+#echo "<script type='text/javascript'>alert({". $update_profile_image_query . "});</script>";
 $query=mysql_query("update profile set image='$destination' where id in (select id from users natural join (select * from profile) as p where username = '$UserName')") or die(mysql_error());
-header("refresh:0;url=manage.php");
+#header("refresh:0;url=manage.php");
 ?>
