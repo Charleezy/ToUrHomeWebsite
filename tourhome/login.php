500811d07612c2a9f734c34ab0e0ea99b4ce8e8e
Steven <stevenspawn@hotmail.com>
2014-07-21 16:22:02 -0400

    login.php update
    login update ui and ux (phase 1)

--- tourhome/login.php
+++ tourhome/login.php
@@ -26,8 +26,95 @@ if ($username && $password) {
 		
 		if ($username == $dbusername && md5($password) == $dbpassword) {
 			
-			echo "You're In! <a href='manage.php'>Click</a> here to enter the member page.";
+			echo "
+			<html>
+			<head>
+			
+				<link rel='stylesheet' href='index.css' type='text/css'>
+				<meta charset='UTF-8'>
+				<link type='text/css' rel='stylesheet' href='bootstrap.css'/>
+				<script type='text/javascript' src='jquery.js'></script>
+				<script type='text/javascript' src='bootstrap.js'></script>
+				<title>ToUrHome</title>
+				
+			</head>
+			
+			<body>
+			<div id='header-container'>
+				<!ToUrHome TITLE>
+					<div id='ToUrHomeTitle'>
+						<a href='http://www.tour-home.org/'>
+							<img src='tourhomelogo.png' width='400' height='80'/>
+						</a>
+			</div>
+        
+			</div>
+	
+			<div id='spacing'> </div>";
+			
+			#echo "<a href='manage.php'>Continue to profile page</a>";
+			
+			echo "<div id='myCarousel' class='carousel slide'>
+				<ol class='carousel-indicators'>
+            <li data-target='#myCarousel' data-slide-to='0' class='active'></li>
+            <li data-target='#myCarousel' data-slide-to='1'></li>
+            <li data-target='#myCarousel' data-slide-to='2'></li>
+				</ol>
+
+			<div class='carousel-inner'>
+					<div class='item active'>
+						<img src='carouselFour.png'/>
+                <div class='container'>
+                    <div class='carousel-caption'>
+                        <h1 id='firstTransition'>logged in as $username ...</h1>
+                        <p><a id='firstButton' class='btn btn-large btn-primary' href='manage.php'>click here to see your profile.</a></p>
+                    </div>
+                </div>
+            </div>";
+      
+
+            echo "<div class='item'>
+                <img src='carouselThree.png'/>
+                <div class='container'>
+                    <div class='carousel-caption'>
+                        <h1 id='secondTransition'>about ToUrHome.</h1>
+                        <p><a id='secondButton' class='btn btn-large btn-danger' href='aboutTourHome.html'>click here to learn more.</a></p>
+                    </div>
+                </div>
+            </div>";
+
+
+            echo "<div class='item'>
+                <img src='carouselOne.png'/>
+                <div class='container'>
+                    <div class='carousel-caption'>
+                        <h1 id='thirdTransition'>behind the scenes.</h1>
+                        <p><a id='thirdButton' class='btn btn-large btn-primary' href='aboutus.html'>click here to meet the creators.</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>";
+
+        echo "<a class='left carousel-control' href='#myCarousel' data-slide='prev'>
+            <span class='glyphicon glyphicon-chevron-left'></span>
+        </a>
+
+        <a class='right carousel-control' href='#myCarousel' data-slide='next'>
+            <span class='glyphicon glyphicon-chevron-right'></span>
+        </a>
+
+    </div>
+    
+    <div id='secondSpacer'></div>
+    <div id='bottom'></div>";
+
+			
+			echo "
+			</body>
+			</html>";
+			
 			$_SESSION['username']=$username;
+			
 			
 		} else
 			echo "Incorrect password!";
