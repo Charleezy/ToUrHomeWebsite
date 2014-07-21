2b35227a9dc9fa0265730ab23b973ba1c977df07
daendinam <d.telatnik@gmail.com>
2014-07-20 21:41:37 -0400

    renamed view, initial form

--- tourhome/viewposts.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-$con = mysqli_connect("mysql.tour-home.org", "stevenpakfunglau", "libertinelux")  or die("Couldn't connect to the database");
-mysqli_select_db($con, "pakfung_phplogin");
-
-?>
